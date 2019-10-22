6fa382f720cbc87d7fb6d9db02d7a33a805da57d
Darryl Ferdinands <darrylferdinands@gmail.com>
2019-10-22 16:39:47 -0700

    Fixed bits of responsiveness
    Working on the navbar and video front content

--- eve-frontend/src/Components/Navbar/NavbarHome.js
+++ eve-frontend/src/Components/Navbar/NavbarHome.js
@@ -8,19 +8,23 @@ class NavbarHome extends Component {
    
     return(
         
-  <nav class="navbar">
-    
+<nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
       <a class="navbar-brand" href="/"><img src="logo.png"></img></a>
     </div>
-    <ul class="nav navbar-nav navbar-right nav-btn">
-   
-      <li><button class="btn navbar-btn navbar-btn1">Create Event</button></li>
-      <li><button class="btn navbar-btn navbar-btn2">Help</button></li>
-      <li><button class="btn navbar-btn navbar-btn3">Login</button></li>
-      
-    </ul>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+          <li><button class="btn navbar-btn navbar-btn1">Create Event</button></li>
+          <li><button class="btn navbar-btn navbar-btn2">Help</button></li>
+          <li><button class="btn navbar-btn navbar-btn3">Login</button></li>
+      </ul>
+    </div>
   </div>
 </nav>
         
